0e9e9fe11726fc67d0115fc8f882da5ccbf2b987
Ryan Fauzi <76712935+ryanfauzi1@users.noreply.github.com>
2021-03-05 03:54:09 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 function ceklogin(){
     session_start();
-    if (($_SESSION['loggedin'] != 1) || isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1500)) {
+    if (($_SESSION['loggedin'] != 1) || isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 9500)) {
     session_unset();
     session_destroy();
     header("Location: login.php");
